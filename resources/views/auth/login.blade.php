11955f2c37a384ad19a838c6e00de23923986647
pengzule <295129789@qq.com>
2016-04-01 17:15:29 +0800

    second

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,21 @@
+<form style="text-align:center" method="post" action="/auth/register">
+ {!! csrf_field() !!}
+
+    <div>
+        Email
+        <input type="email" name="email" value="{{ old('email') }}">
+    </div>
+
+    <div>
+        密码
+        <input type="password" name="password" id="password">
+    </div>
+
+    <div>
+        <input type="checkbox" name="remember"> 记住我
+    </div>
+
+    <div>
+        <button type="submit">登录</button>
+    </div>
+</form>
